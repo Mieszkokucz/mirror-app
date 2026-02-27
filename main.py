69149261abe83433acdc916965ba2a246bb3626f
Mieszkokucz <56246624+Mieszkokucz@users.noreply.github.com>
2026-02-27 19:35:35 +0100

    Add GET, PATCH, DELETE /reflections/ endpoint with Pydantic schema

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from database import SessionLocal
 from sqlalchemy.orm import Session
 from models import DailyReflection
-from schemas import DailyReflectionCreate
+from schemas import (
+    DailyReflectionCreate,
+    DailyReflectionResponse,
+    DailyReflectionUpdate,
+)
+import uuid
 
 
 app = FastAPI()
@@ -14,11 +19,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-@app.get("/")
-def first_endpoint(db: Session = Depends(get_db)):
-    return {"status": "ok"}
 
 
 @app.post("/reflections/")
@@ -37,3 +37,55 @@ def create_daily_reflection(
     db.refresh(db_reflection)
 
     return {"status": "ok"}
+
+
+@app.get("/reflections/", response_model=list[DailyReflectionResponse])
+def read_daily_reflection(user_id: uuid.UUID, db: Session = Depends(get_db)):
+    return db.query(DailyReflection).filter(DailyReflection.user_id == user_id).all()
+
+
+@app.get("/reflections/{reflection_id}", response_model=DailyReflectionResponse)
+def read_daily_reflection_by_id(
+    reflection_id: uuid.UUID, db: Session = Depends(get_db)
+):
+    reflection = (
+        db.query(DailyReflection).filter(DailyReflection.id == reflection_id).first()
+    )
+    if reflection is None:
+        raise HTTPException(status_code=404, detail="Reflection not found")
+    return reflection
+
+
+@app.patch("/reflections/{reflection_id}", response_model=DailyReflectionResponse)
+def update_daily_reflection(
+    reflection_id: uuid.UUID,
+    new_data: DailyReflectionUpdate,
+    db: Session = Depends(get_db),
+):
+    reflection = (
+        db.query(DailyReflection).filter(DailyReflection.id == reflection_id).first()
+    )
+    if reflection is None:
+        raise HTTPException(status_code=404, detail="Reflection not found")
+
+    update_data = new_data.model_dump(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(reflection, key, value)
+
+    db.commit()
+    db.refresh(reflection)
+
+    return reflection
+
+
+@app.delete("/reflections/{reflection_id}", status_code=204)
+def delete_daily_reflection(reflection_id: uuid.UUID, db: Session = Depends(get_db)):
+    reflection = (
+        db.query(DailyReflection).filter(DailyReflection.id == reflection_id).first()
+    )
+    if reflection is None:
+        raise HTTPException(status_code=404, detail="Reflection not found")
+
+    db.delete(reflection)
+    db.commit()
--- schemas.py
+++ schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 import uuid
 import datetime
+from typing import Optional
 
 
 class DailyReflectionCreate(BaseModel):
@@ -8,3 +9,22 @@ class DailyReflectionCreate(BaseModel):
     reflection_type: str
     content: str
     date: datetime.date
+
+
+class DailyReflectionResponse(BaseModel):
+    id: uuid.UUID
+    user_id: uuid.UUID
+    reflection_type: str
+    date: datetime.date
+    content: str
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+
+    class Config:
+        from_attributes = True
+
+
+class DailyReflectionUpdate(BaseModel):
+    reflection_type: Optional[str] = None
+    content: Optional[str] = None
+    date: Optional[datetime.date] = None
